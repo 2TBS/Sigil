6babe9c1c4baaa70153dc4b26309024e3936f3b5
Ben Cuan <ben.cuan@gmail.com>
2018-01-05 20:47:41 -0800

    Random spawn generation

---
+++ Assets/Scripts/Button.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour {
+
+	///0 through 4. Type of tile.
+	private int type;
+	public GameObject[] tileObjects;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void UpdateItem() {
+		try {
+			Destroy(GetComponentsInChildren<GameObject>()[1]);
+		} catch {} finally {
+			GameObject newItem = Instantiate(tileObjects[type], gameObject.transform.position, Quaternion.identity);
+            newItem.transform.SetParent(transform);
+		}
+		
+	}
+	
+	public void SetType(int type) {
+		this.type = type;
+	}
+}
---
+++ Assets/Scripts/Spawn.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+///Attach to Board
+public class Spawn : MonoBehaviour {
+
+	public GameObject board;
+	public Button[] tiles;
+
+	// Use this for initialization
+	void Start () {
+
+		tiles = board.GetComponentsInChildren<Button>();
+		foreach(Button tile in tiles) {
+			tile.SetType(Random.Range(0,4));
+			tile.UpdateItem();
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
