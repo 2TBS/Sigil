943440a44e35932f1867a904a2b3def774e5ed56
Akash Basu <33015666+FugaX@users.noreply.github.com>
2018-01-14 19:13:45 -0800

    Main Menu with Scripting
    This is the Main Menu along with the art and the scripting for it.

---
+++ Assets/Scripts/M_Menu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class M_Menu : MonoBehaviour {
+
+    int change = 0;
+    int level = 0; 
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void LoadScene()
+    {
+        level++;
+        SceneManager.LoadScene(level);
+       
+    }
+}
